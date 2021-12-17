c9b6409b73b578c67977828ad2737ccd6227ce32
Tobias Schneider <tobias.schneider@smail.inf.h-brs.de>
2021-12-17 17:19:16 +0100

    make out_path optional

--- voltcraft_el_parser/__main__.py
+++ voltcraft_el_parser/__main__.py
@@ -1,4 +1,4 @@
-from energylogger_parser import app
+from voltcraft_el_parser import app
 
 if __name__ == '__main__':
     app.run()
--- voltcraft_el_parser/app.py
+++ voltcraft_el_parser/app.py
@@ -1,4 +1,5 @@
 import argparse
+from os.path import basename, splitext
 
 from voltcraft_el_parser.binary_parser import parse_file
 from voltcraft_el_parser.writer import export_csv
@@ -9,8 +10,14 @@ def run():
     parser = argparse.ArgumentParser(description="A parser for EL3500/EL4000 binary files",
                                      prog="python -m voltcraft_el_parser")
     parser.add_argument("binary_path", type=str, help="path to the input binary file")
-    parser.add_argument("out_path", type=str, help="path to create an output .csv file")
+    parser.add_argument("out_path", nargs='?', type=str, help="path for output .csv file (optional)")
     args = vars(parser.parse_args())
 
-    export_csv(parse_file(args['binary_path']), args['out_path'])
-    print(args['out_path'], 'was sucessfully created!')
+    # determine output path
+    out_path = args['out_path']
+    if not out_path:
+        out_path = splitext(basename(args['binary_path']))[0] + '.csv'
+
+    # export to csv
+    export_csv(parse_file(args['binary_path']), out_path)
+    print(out_path, 'was sucessfully created!')
