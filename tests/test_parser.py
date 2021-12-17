7d4aa9651a385cb9df20a54960f085f2f7a3d1c1
Tobias Schneider <tobias.schneider@smail.inf.h-brs.de>
2021-12-17 19:04:52 +0100

    add simple test-case

---
+++ tests/test_parser.py
@@ -0,0 +1,28 @@
+import filecmp
+import os
+import pathlib
+
+from voltcraft_el_parser.binary_parser import parse_file
+from voltcraft_el_parser.writer import export_csv
+
+
+def test_parser():
+    """ simple test-case that tests the parsing against a pre-parsed file """
+
+    data_directory = pathlib.Path(__file__).parent / 'data'
+
+    file_in = data_directory / 'b1c622b2.bin'
+    file_out = data_directory / 'tmp.csv'
+    file_compare = data_directory / 'expected.csv'
+
+    # parse binary file
+    export_csv(parse_file(file_in), file_out)
+    filecmp.clear_cache()
+    result = filecmp.cmp(file_compare, file_out, shallow=False)
+    print("res:", result)
+
+    # cleanup
+    os.remove(file_out)
+
+    # compare parsed file to expected file
+    assert result, 'parsed file is not equal to expected file'
